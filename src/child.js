b5eb7e18feb90a69759e34c3ba9561d724f12e8f
azm98 <49077908+azm98@users.noreply.github.com>
2020-08-27 14:14:03 -0700

    intialize expense tracker app

---
+++ src/child.js
@@ -0,0 +1,100 @@
+import React, { useContext, useState } from 'react';
+import { TransactionContext } from './transContext';
+
+
+function Child() {
+    let { transactions, addTransaction,deleteTransaction} = useContext(TransactionContext);
+    let [newDesc, setDesc] = useState("");
+    let [newAmount, setAmount] = useState(0);
+
+
+    const handleAddition = (event) => {
+        event.preventDefault();
+        if (Number(newAmount) === 0) {
+            alert("Please enter correct value");
+            return false;
+        }
+        addTransaction({
+            amount: Number(newAmount),
+            desc: newDesc
+        });
+
+        setDesc('');
+        setAmount(0)
+    }
+
+    const getIncome = () => {
+        let income = 0;
+        for (var i = 0; i < transactions.length; i++) {
+            if (transactions[i].amount > 0)
+                income = income + transactions[i].amount
+        }
+        return income;
+    }
+
+    const getExpense = () => {
+        let expense = 0;
+        for (var i = 0; i < transactions.length; i++) {
+            if (transactions[i].amount < 0)
+                expense += transactions[i].amount
+        }
+        return expense;
+    }
+    return (
+        <div className="container">
+            <h1 className="text-center">Expense Tracker</h1>
+
+            <h3>Your Balance <br /> Rs.{getIncome() + getExpense()}</h3>
+
+            <div className="expense-container">
+                <h3>INCOME <br /> Rs.{getIncome()}</h3>
+                <h3>EXPENSE <br /> Rs.{getExpense()}</h3>
+            </div>
+
+            <h3>History</h3>
+            <hr />
+
+            <ul className="transaction-list">
+                {transactions.map((transObj, ind) => {
+                    return (<li key={ind} onClick={()=>deleteTransaction({id: transObj.id})}>
+                        <span>{transObj.desc}</span>
+                        <span>Rs.{transObj.amount}</span>
+                    </li>
+                    )
+                })}
+              
+           
+            </ul>
+
+            <h3>Add new transaction</h3>
+            <hr />
+
+            <form className="transaction-form" onSubmit={handleAddition}>
+                <label>
+                    Enter Description <br />
+                    <input id="text"
+                        type="text"
+                        value={newDesc}
+                        placeholder="Description"
+                        onChange={(ev) => setDesc(ev.target.value)}
+                        required />
+                </label>
+
+                <br />
+                <label>
+                    Enter Amount <br />
+                    <input type="number"
+                        value={newAmount}
+                        placeholder="Amount"
+                        onChange={(ev) => setAmount(ev.target.value)}
+                        required />
+                </label>
+                <br />
+                <input className="btn"
+                    type="submit" value="Add Transaction" />
+            </form>
+        </div>
+    );
+}
+
+export default Child;
